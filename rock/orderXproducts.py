d79c54dbc944aed0772c2a082dc3ea68fd018806
Rockheung <rockheung@gmail.com>
2017-08-07 06:19:20 +0900

    update corr

--- rock/orderXproducts.py
+++ rock/orderXproducts.py
@@ -17,8 +17,8 @@ products = pd.read_csv(os.path.join(IDIR,'products.csv'),
 
 print('products loaded')
 
-oXp = oXp[:1024]
-products = products[:10240]
+#oXp = oXp[:1024]
+#products = products[:10240]
 
 oXp2 = oXp.groupby('order_id').apply(lambda x: [x[i].tolist() for i in x ][1] )
 
@@ -26,26 +26,39 @@ print('Grouping oXp Colplete')
 
 oXp3 = pd.DataFrame(index=oXp2.index)
 
-for j in products.product_id:
-    oXp3[j] = False
+#  for i in oXp2.index:
+#      for j in products.product_id:
+#          try:
+#              oXp3.loc[i][j] = True
+#          except:
+#              oXp3[j] = False
+#              oXp3.loc[i][j] = True
+print(oXp3)
 
 print('Fill False Complete')
-
+print(oXp2.index)
 for i in oXp2.index:
     for j in products.product_id:
-        if j in oXp2[i]: oXp3.loc[i][j] = True
+        if j in oXp2[i]:
+            try:
+                oXp3[i][j] = True
+            except:
+                oXp3[j] = False
+                oXp3.loc[i][j] = True
+                print(i, j)
 #        else: oXp3.loc[i][j] = False
     print(i, "th row checked")
 
 
-oXp3.to_csv(os.path.join(IDIR,'hello_himyp.csv'))
+#oXp3.to_csv(os.path.join(IDIR,'hello_himyp.csv'))
 
-oXp3_corr = oXp3.corr().to_csv(os.path.join(IDIR,'corr_himyp.csv'))
-
-for i in products.product_id:
-    if np.isnan(oXp3_corr[i]):
-        oXp3_corr = oXp3_corr.drop(i)
-
+oXp3_corr = oXp3.corr()
+# .to_csv(os.path.join(IDIR,'corr_himyp.csv'))
+print(oXp3_corr)
+# for i in products.product_id:
+#     if np.isnan(oXp3_corr[i]):
+#         oXp3_corr = oXp3_corr.drop(i)
+# 
 # for j in products.product_id:
 #     if oXp3_corr[j] == np.nan()
 #         oXp3_corr.drop(i)
